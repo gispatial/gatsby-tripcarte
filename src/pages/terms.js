380402773a23d083068a198983c04f7377294973
Spatial7Host <bitbucket@gispatial.tech>
2020-02-25 11:02:40 +0800

    Initial commit

---
+++ src/pages/terms.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import SEO from '../components/SEO'
+import Layout from '../components/Layout'
+
+const Terms = ({location}) => (
+  <Layout location={location}>
+    <SEO title="Terms" />
+    <h1>Terms</h1>
+    <p>
+      Tripcarte Asia Sdn Bhd (‘Tripcarte Asia’, ‘we’, ‘us’, ‘our’) are committed to protecting and respecting
+      your personal data privacy and complying with data protection principles and provisions under applicable laws.
+      We may collect, process, use and disclose your information when you use this website
+      and the services offered by Tripcarte Asia through this Website (the ‘Services’)
+      as described in this Privacy Policy. ‘You’ and ‘your’ when used in this Privacy Policy
+      includes any person who accesses this Website or use the Services.
+      This Privacy Policy sets out the basis and terms upon which Tripcarte Asia collects,
+      processes, uses and/or discloses your information that is obtained from
+      you when you access this Website and/or use the Services. Such information may include personal information relating to or linked to a specific individual such as name, residential address, telephone number, email address, travel document information or any such information
+      we have requested and you have provided through this Website (‘Personal Information’).
+    </p>
+  </Layout>
+)
+
+export default Terms
